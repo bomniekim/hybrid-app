c0d9492f426a025139b8b00c0c1aa348402d2159
Bomin Kim <bomnie.kim@gmail.com>
2020-01-22 15:43:47 +0900

    Image/ ImageBackground/ Touchable~/ ScrollView

--- ReactNative/RN05Image/MainComponent.js
+++ ReactNative/RN05Image/MainComponent.js
@@ -3,25 +3,25 @@ import {Text, View, Button, Image, TouchableHighlight, TouchableOpacity, Touchab
 
 export default class MainComponent extends Component{
 
-    // constructor(){
-    //     super();
+    constructor(){
+        super();
 
-    //     // 컴포넌트의 특별한 멤버변수 state
-    //     this.state={
-    //         img: require('./images/ben1.jpg'),
+        // 컴포넌트의 특별한 멤버변수 state
+        this.state={
+            img: require('./images/ben1.jpg'),
 
-    //         // 이미지를 순차적으로 변경하기 위해
-    //         imgNum: 0, // 배열의 index 번호
-    //         imgArray: [ // 함수도 객체이므로 JS의 배열 안에 넣을 수 있음
-    //             require('./images/ben1.jpg'),  
-    //             require('./images/ben2.jpg'),  
-    //             require('./images/ben3.jpg'),
-    //             //네트워크 이미지도 당연히 사용가능
-    //             { uri: 'https://pbs.twimg.com/media/EJlJ_DNU8AA37Bw.jpg'}  
-    //         ]  
+            // 이미지를 순차적으로 변경하기 위해
+            imgNum: 0, // 보여줄 이미지를 가진 배열의 index 번호
+            imgArray: [ // 함수도 객체이므로 JS의 배열 안에 넣을 수 있음
+                require('./images/ben1.jpg'),  
+                require('./images/ben2.jpg'),  
+                require('./images/ben3.jpg'),
+                //네트워크 이미지도 당연히 사용가능
+                { uri: 'https://pbs.twimg.com/media/EJlJ_DNU8AA37Bw.jpg'}  
+            ], 
 
-    //     };
-    // }
+        };
+    }
 
     render(){
         return(
@@ -46,58 +46,76 @@ export default class MainComponent extends Component{
             //     </Image>
             // </View>
 
+
             // 4) 이미지 클릭 반응하기: Button이 아니면 onPress 속성은 사용할 수 없음
             // 터치를 인지하도록 만들어진 컴포넌트들 (TouchableXXX)을 사용
-            //         <TouchableHighlight onPress={this.clickImage}>
-            //             <Image
-            //                 style={{ width: 150, height: 150, resizeMode: 'cover' }}
-            //                 source={require("./images/pengsoo2.jpg")}>
-            //             </Image>
-            //         </TouchableHighlight>
-            //         {/* Button이 아닌 컴포넌트들을 클릭하고 싶을 때 이 방법으로 모두 사용 가능 */}
+            // Button이 아닌 컴포넌트들을 클릭하고 싶을 때 이 방법으로 모두 사용 가능
+            
+            // 4-1) TouchableHighlight
+            // <TouchableHighlight onPress={this.clickImage}>
+            //     <Image
+            //         style={{ width: 150, height: 150, resizeMode: 'cover' }}
+            //         source={require("./images/pengsoo2.jpg")}>
+            //     </Image>
+            // </TouchableHighlight>
 
-            //         {/* <TouchableOpacity>
-            //         <Image
-            //             style={{ width: 150, height: 150, resizeMode: 'cover' }}
-            //             source={require("./images/pengsoo3.jpg")}>
-            //         </Image>
-            //     </TouchableOpacity> */}
+            // 4-2) TouchableOpcity
+            // <TouchableOpacity>
+            //     <Image
+            //         style={{ width: 150, height: 150, resizeMode: 'cover' }}
+            //         source={require("./images/pengsoo3.jpg")}>
+            //     </Image>
+            // </TouchableOpacity> */}
 
-            //         {/* 이미지 바꾸기 */}
-            //         {/* RN에서는 이미지를 보여주는 객체를 변수로 만들고 변수값을 변경해서 보여주는 방법을 사용 */}
-            //         <TouchableOpacity onPress={ this.changeImage }>
-            //         <Image
-            //             style={{ width: 150, height: 150, resizeMode: 'cover' }}
-            //             source={ this.state.imgArray[this.state.imgNum] }>
-            //         </Image>
-            //     </TouchableOpacity>
+            // 4-3) TouchableNativeFeedback: only for Android
+            // <TouchableNativeFeedback 
+            //      background={TouchableNativeFeedback.SelectableBackground()>
+            //     <Image
+            //         style={{ width: 150, height: 150, resizeMode: 'cover' }}
+            //         source={require("./images/pengsoo3.jpg")}>
+            //     </Image>
+            // </TouchableNativeFeedback> */}
+           
+            // 4-4) TouchableWithoutFeedback: 클릭 시 아무런 효과가 발생하지 않도록
+            // <TouchableWithoutFeedback onPress={ this.changeImage }>
+            //     <Image
+            //         style={{ width: 150, height: 150, resizeMode: 'cover' }}
+            //         source={ this.state.imgArray[this.state.imgNum] }>
+            //     </Image>
+            // </TouchableWithoutFeedback>
 
-            //         {/* 클릭 시 아무런 효과가 발생하지 않도록 */}
-            //         <TouchableWithoutFeedback onPress={ this.changeImage }>
-            //         <Image
-            //             style={{ width: 150, height: 150, resizeMode: 'cover' }}
-            //             source={ this.state.imgArray[this.state.imgNum] }>
-            //         </Image>
-            //     </TouchableWithoutFeedback>
 
-            //         {/* <TouchableNativeFeedback onPress={this.changeImage}
-            //         background={TouchableNativeFeedback.SelectableBackground()}>
-            //         <Image
-            //             style={{ width: 150, height: 150, resizeMode: 'contain' }}
-            //             source={this.state.imgArr[this.state.imgNum]}>
-            //         </Image>
-            //     </TouchableNativeFeedback> */}
 
-            //     </ScrollView>
+            // 5) 이미지 클릭 시 다른 이미지로 전환
+            // RN에서는 이미지를 보여주는 객체를 변수로 만들고 변수값을 변경해서 보여주는 방법을 사용
+            <TouchableOpacity onPress={this.changeImage}>
+                <Image
+                    style={{ width: 150, height: 150, resizeMode: 'cover' }}
+                    source={this.state.imgArray[this.state.imgNum]}>
+                </Image>
+            </TouchableOpacity>
 
-           // </View>
-            <View>
-                <ImageBackground source={ require('./images/pengsoo.jpg')} style={ {width:'100%', height:'100%'} }>
-                    <Text style={ {color:'white', fontSize:23, fontWeight:'bold', margin:16}}>Image</Text>
-                    <Text style={ {color:'navy', fontSize:24, margin:8}}>Pengsoo</Text>
 
-                </ImageBackground>
-            </View>
+            // 6) ScrollView: content가 많은 경우
+            // <ScrollView>
+            //     <Image source={require('./images/ben1jpg')} sytle={{width:120, height:100, margin:8}}></Image>
+            //     <Image source={require('./images/ben2.jpg')} sytle={{width:120, height:100, margin:8}}></Image>
+            //     <Image source={require('./images/ben3.jpg')} sytle={{width:120, height:100, margin:8}}></Image>
+            //     <Image source={require('./images/pengsoo.jpg')} sytle={{width:120, height:100, margin:8}}></Image>
+            //     <Image source={require('./images/pengsoo2.jpg')} sytle={{width:120, height:100, margin:8}}></Image>
+            //     <Image source={require('./images/pengsoo3.jpg')} sytle={{width:120, height:100, margin:8}}></Image>
+            // </ScrollView>
+
+
+            // 7) 배경이미지 적용하기 (css의 background-image)
+            // <ImageBackground> 사용
+            // <View>
+            //     <ImageBackground source={ require('./images/pengsoo.jpg')} style={ {width:'100%', height:'100%'} }>
+            //         <Text style={ {color:'white', fontSize:23, fontWeight:'bold', margin:16}}>Image</Text>
+            //         <Text style={ {color:'navy', fontSize:24, margin:8}}>Pengsoo</Text>
+            //     </ImageBackground>
+            // </View>
+            
         );
     }
 
@@ -110,6 +128,7 @@ export default class MainComponent extends Component{
         let num= this.state.imgNum;
         num++;
 
+        // index가 4가 되면 다시 0으로 돌아가도록
         if(num>4) num=0;
         this.setState({imgNum: num});
 
