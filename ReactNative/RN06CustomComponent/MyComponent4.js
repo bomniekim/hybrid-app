2523c645c4affd58e9c1c5ec87b659676840d78d
Bomin Kim <bomnie.kim@gmail.com>
2020-02-09 04:21:01 +0900

    구조분해할당의 스프레드 연산자를 통해 속성(props) 한번에 적용하기

--- ReactNative/RN06CustomComponent/MyComponent4.js
+++ ReactNative/RN06CustomComponent/MyComponent4.js
@@ -7,11 +7,10 @@ export default class MyComponent4 extends Component{
             <View style={{margin:16}}>
                 {/* 전달받을 속성을 한번에 적용하기 */}
                 {/* 스프레드 연산자: ... / 구조분해할당 */}
+                {/* 매개변수로 다중 argument를 전달받을 때 사용 */}
+                {/* 파라미터로 배열을 받을 때와 다르게 0개짜리 배열도 전달받을 수 있음 */}
                 <Button {...this.props}></Button>
             </View>
         );
     }
 }
-
-
-// commit: 구조분해할당의 스프레드 연산자를 통해 속성 한번에 적용하기 
