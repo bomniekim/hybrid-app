fb03b2f38682972bd8a725fd1beffa387b70f278
Bomin Kim <bomnie.kim@gmail.com>
2020-02-09 23:20:38 +0900

    대량의 데이터를 배열에 저장하고 map()을 통해  리스트 형태로 보여주기

--- ReactNative/RN07ListLayout/MainComponent.js
+++ ReactNative/RN07ListLayout/MainComponent.js
@@ -6,7 +6,10 @@ export default class MainComponent extends Component{
     render(){
         
         // 지역변수: 태그문 문자열을 저장한 것이 아니라 Text컴포넌트를 저장한 것
+        // 즉, 변수에 컴포넌트 객체 자체를 저장
         const aaa= <Text>Nice</Text>;
+
+        // 여러 뷰를 가진 뷰도 저장 가능
         const bbb= <View style={ {marginTop:16}}>
                         <Text>Hello React Native</Text>
                         <Button title="button"></Button>
@@ -17,6 +20,7 @@ export default class MainComponent extends Component{
 
         // 원래 배열을 출력하면 각 요소를 구별하는 key속성이 없으면 경고
         // 그래서 key값을 부여했어야 했음 - 이제는 경고창 안뜨는걸 보니 괜찮나봉가
+        // 속성명 'key'는 고정
         const array2=[
             <View key="k1">{aaa}</View>,
             <View key="k2">{bbb}</View>,
@@ -28,27 +32,45 @@ export default class MainComponent extends Component{
         let data= ["Changmo", "Leella", "Hash"];
 
         return(
+
+            // 1. 변수에 컴포넌트를 저장한 후 사용하기 (여러 뷰를 가진 뷰를 저장한 변수도 물론 가능)
+            // <View style={style.root}>
+            //     <Text style={style.title}>List Layout Test</Text>
+            //     {/* JSX에서 JS 변수를 사용하려면 {} 안에 작성 */}
+            //     { aaa }
+            //     { bbb }
+            // </View>
+
+
+            // 2. 메소드를 호출하여 컴포넌트를 리턴하여 사용하기
+            // <View style={style.root}>
+            //     <Text style={style.title}>List Layout Test</Text>
+
+            //     {/* 멤버메소드 호출 */}
+            //     { this.showItemView() }
+
+            //     {/* 메소드 호출 시 파라미터 전달도 가능 */}
+            //     { this.showItemView2("Genie", "press me", "indigo") }
+            // </View>
+
+            // 3. 배열변수에 컴포넌트를 요소로 넣어 사용하기 
             <View style={style.root}>
                 <Text style={style.title}>List Layout Test</Text>
-                { aaa }
-                { bbb }
-
-                {/* 메소드 호출 */}
-                { this.showItemView() }
-                { this.showItemView2("Genie", "press me", "indigo") }
-
                 {/* 배열에 저장된 컴포넌트들 출력 */}
                 {/* { array } */}
+                {/* 배열변숨여을 작성하기만 하면 자동으로 안에 있는 요소들의 값들이 출력됨 */}
                 {/* 원래는 배열의 요소들을 구분하는 식별자 key 속성이 없다고 경고창이 뜸 */}
 
                 { array2 }
 
                 {/* 데이터 자체만으로는 컴포넌트가 아니므로 직접 출력이 불가 */}
                 {/* 데이터(string)의 개수만큼 <Text>를 만들고 그 안에 데이터를 출력 */}
-                {/* 배열객체의 map(): 배열의 요소개수만큼 반복하면서 그 값을 새로운 배열로 리턴 */}
+                {/* 배열객체의 map(): 배열의 요소 개수만큼 반복하면서 그 값을 새로운 배열로 리턴 */}
+                {/* 즉, 배열 개수만큼 리턴하여 새로운 배열이 만들어짐 */}
                 {
                     data.map( function(value, index, array){
                         return (
+                            // 배열요소들의 값을 구별하는 정해진 식별자 key props 적용
                             <View key={index} style={{margin:16}}>
                                 <Text>{ value }</Text>
                             </View>
@@ -58,6 +80,9 @@ export default class MainComponent extends Component{
 
                 
             </View>
+
+            // 단순 문자열뿐만 아닌 객체들로서 item의 내용을 꾸밀 수 있음
+            // RN08 예제로..
         );
     }
 
@@ -71,6 +96,8 @@ export default class MainComponent extends Component{
         );
     
     }
+
+    // 파라미터를 전달받는 메소드
     showItemView2(name, title, color){
         return(
             <View style={{marginTop:16}}>
