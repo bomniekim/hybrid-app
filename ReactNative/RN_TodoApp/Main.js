196ca18712726a482a2fc837c9bc671e8dc1ace0
Bomin Kim <bomnie.kim@gmail.com>
2020-02-14 21:42:26 +0900

    making editing function

--- ReactNative/RN_TodoApp/Main.js
+++ ReactNative/RN_TodoApp/Main.js
@@ -1,94 +1,100 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, TextInput, Dimensions, Platform, ScrollView} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView,
+} from 'react-native';
 
 import LinearGradient from 'react-native-linear-gradient';
 
 import ToDo from './ToDo';
 
-
 const {height, width} = Dimensions.get('window');
 
-export default class Main extends Component{
-    constructor(){
-        super();
+export default class Main extends Component {
+  constructor() {
+    super();
 
-        this.state={
-            addToDo:"",
-        }
-    }
-    render(){
+    this.state = {
+      addToDo: '',
+    };
+  }
+  render() {
+    const {addToDo} = this.state; // 구조분해할당
 
-        const {addToDo} = this.state; // 구조분해할당
+    return (
+      <LinearGradient colors={['#FAF4C0', '#8BBCFF']} style={styles.container}>
+        <Text style={styles.title}>Cool To Do</Text>
+        <View style={styles.card}>
+          <TextInput
+            style={styles.input}
+            placeholder={'Add to do'}
+            value={addToDo}
+            onChangeText={this._addToDo}
+            placeholderTextColor={'#999'}
+            // returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo text={"Hello I'm ToDo"}></ToDo>
+          </ScrollView>
+        </View>
+      </LinearGradient>
+    );
+  }
 
-        return(
-            <LinearGradient colors={["#FAF4C0", "#8BBCFF"]} style={styles.container}>
-                <Text style={styles.title}>Cool To do</Text>
-                <View style={styles.card}>
-                    <TextInput
-                    style={styles.input}
-                    placeholder={"Add to do"}
-                    onChangeText={this._addToDo}
-                    placeholderTextColor={'#999'}
-                    // returnKeyType={"done"}
-                    autoCorrect={false}/>
-                    <ScrollView contentContainerStyle={styles.toDos}>
-                        <ToDo></ToDo>
-                    </ScrollView>
-                </View>
-            </LinearGradient>
-        )
-    }
-
-    _addTodo= text =>{
-        this.setState({
-            addToDo: text // 파라미터로 받아온 text로 setState 
-        })
-    }
+  _addTodo = text => {
+    this.setState({
+      addToDo: text, // 파라미터로 받아온 props인 text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#f23657',
-        alignItems:'center',
-    },
-    title:{
-        color:'black',
-        fontSize:30,
-        fontWeight:'100',
-        marginTop:30,
-        marginBottom:20,
-    },
-    card:{
-        backgroundColor:'white',
-        flex:1,
-        width: width-50,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-        ...Platform.select({
-            ios:{
-                shadowColor:"rgb(50, 50, 50)",
-                shadowOpacity: 0.5,
-                shadowOffset:{
-                    height: -1,
-                    width:0
-                }
-            },
-            android:{
-                elevation: 5,
-
-            }
-        })
-    },
-    input:{
-        padding:16,
-        borderBottomColor:'#bbb',
-        // borderBottomWidth: StyleSheet.hairlineWidth
-        borderBottomWidth: 1,
-        fontSize:18,
-    },
-    toDos:{
-        alignItems: 'center',
-    },
-
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#f23657',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'black',
+    fontSize: 35,
+    fontWeight: '100',
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 50,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowOffset: {
+          height: -1,
+          width: 0,
+        },
+      },
+      android: {
+        elevation: 10,
+      },
+    }),
+  },
+  input: {
+    padding: 16,
+    borderBottomColor: '#bbb',
+    // borderBottomWidth: StyleSheet.hairlineWidth
+    borderBottomWidth: 1,
+    fontSize: 18,
+  },
+  toDos: {
+    alignItems: 'center',
+  },
+});
--- ReactNative/RN_TodoApp/ToDo.js
+++ ReactNative/RN_TodoApp/ToDo.js
@@ -1,73 +1,168 @@
 import React, {Component} from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  TextInput,
+} from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
-export default class ToDo extends Component{
+export default class ToDo extends Component {
+  state = {
+    isEditing: false,
+    isCompleted: false,
+    toDoValue: '', // edit할 경우 해당 텍스트를 복사해 state에 저장
+  };
+  render() {
+    const {isCompleted, isEditing, toDoValue} = this.state;
+    const {text} = this.props;
 
-    state={
-        isEditing: false,
-        isCompleted: false,
-    }
-    render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.column}>
+          <TouchableOpacity onPress={this._toggleCompleteTodo}>
+            <View
+              style={[
+                styles.circle,
+                isCompleted ? styles.completedCircle : styles.uncompletedCircle,
+              ]}
+            />
+            {/* 스타일을 배열로 작성해 여러 개를 줄 수 있음 */}
+          </TouchableOpacity>
+          {isEditing ? (
+            <TextInput
+              style={[
+                styles.input,
+                styles.text,
+                isCompleted ? styles.completedText : styles.uncompletedText,
+              ]}
+              value={toDoValue}
+              multiline={true}
+            />
+          ) : (
+            <Text
+              style={[
+                styles.text,
+                isCompleted ? styles.completedText : styles.uncompletedText,
+              ]}>
+              {text}
+            </Text>
+          )}
+        </View>
 
-        const {isCompleted} = this.state;
+        {isEditing ? (
+          <View style={styles.action}>
+            <TouchableOpacity onPressOut={this._finishEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionChecked}>✓</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.action}>
+            <TouchableOpacity onPressOut={this._startEditing}>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>✏️</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.actionContainer}>
+                <Text style={styles.actionText}>❌</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    );
+  }
 
-        return(
-            <View style={styles.container}>
-                <TouchableOpacity onPress={this._toggleCompleteTodo} >
-                    <View style={
-                        [styles.circle,
-                        isCompleted? styles.completedCircle : styles.uncompletedCircle]} />
-                    {/* 스타일을 배열로 작성해 여러 개를 줄 수 있음 */}
-                </TouchableOpacity>
-                <Text style={styles.text}>Hello I'm Todo</Text>
-            </View>
-        );
-    }
+  _toggleCompleteTodo = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted: !prevState.isCompleted,
+        // 전의 boolean 값과 반대로 toggle
+        // 완료되지 않은 상태에서 누르면 완료되고, 완료된 상태에서 누르면 완료되지 않은 상태로 돌아감
+      };
+    });
+  };
 
-    _toggleCompleteTodo = ()=>{
-        this.setState(prevState =>{
-            return{
-                isCompleted: !prevState.isCompleted
-                // 전의 boolean 값과 반대로 toggle
-                // 완료되지 않은 상태에서 누르면 완료되고, 완료된 상태에서 누르면 완료되지 않은 상태로 돌아감
-            };
+  _startEditing = () => {
+    const {text} = this.props;
+    this.setState({
+      isEditing: true,
+      toDoValue: text, // 편집을 시작하면 props에서 text를 가져와서 이를 state에 저장
+    });
+  };
 
-        });
-    };
+  _finishEditing = () => {
+    this.setState({
+      isEditing: false,
+    });
+  };
 }
 
 const styles = StyleSheet.create({
-    container:{
-        width: width -70,
-        borderBottomColor: '#bbb',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        flexDirection:'row',
-        alignItems:"center",
-
-    },
-    circle:{
-        width: 30,
-        height: 30,
-        borderRadius: 15,
-        borderColor:"red",
-        borderWidth: 4,
-        marginRight:15,
-        marginLeft:5,
-    },
-    completedCircle:{
-        borderColor:"#bbb"
-
-    },
-    uncompletedCircle:{
-        borderColor: "#8BBCFF",
-
-    },
-    text:{
-        fontWeight:'bold',
-        fontSize:20,
-        marginVertical: 15,
-    },
-})
-
+  container: {
+    width: width - 70,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  column: {},
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: 'red',
+    borderWidth: 4,
+    marginRight: 15,
+    marginLeft: 5,
+  },
+  completedCircle: {
+    borderColor: '#bbb',
+  },
+  uncompletedCircle: {
+    borderColor: '#8BBCFF',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 23,
+    marginVertical: 15,
+  },
+  completedText: {
+    color: '#ddd',
+    textDecorationLine: 'line-through',
+  },
+  uncompletedText: {
+    color: '#353535',
+  },
+  column: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: width / 2,
+  },
+  action: {
+    flexDirection: 'row',
+  },
+  actionContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    // marginVertical: 10,
+    // marginHorizontal: 10,
+    // 아이콘이 아닌 근처를 클릭해도 감지할 수 있도록 padding을 주는 것
+    // backgroundColor: 'tomato',
+  },
+  actionChecked: {
+    fontSize: 30,
+  },
+  actionText: {},
+  input: {
+    marginVertical: 10,
+  },
+});
